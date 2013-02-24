ad0982060f0c555230704e8e3fc50af6bf000890
David Nelson <david.jonathan.nelson@gmail.com>
2013-02-24 02:42:48 -0800

    made the app minifiable

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,12 +1,10 @@
-var app = angular.module('app', []);
-
-app.config(function ($locationProvider, $routeProvider) {
+angular.module('app', [])
+.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when("/", {template:"<h2>Welcome to the Crew Explorer</h2>"}).
         when("/Enterprise-D", {template: "<h2>Enterprise D!</h2>"}).
         when("/Voyager", {template: "<h2>Voyager!</h2>"});
-});
-
-function AppCtrl($scope, $rootScope, $location) {
+}])
+.controller('AppCtrl', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location) {
     $rootScope.$on("$routeChangeSuccess", function () {
         $scope.newLocation = $location.path();
     });
@@ -17,5 +15,5 @@ function AppCtrl($scope, $rootScope, $location) {
         } else {
             return "";
         }
-    };
-}
+    }
+}]);
